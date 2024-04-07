01ee8cdb79168cb728fd828ca1659c75eaf1eeb0
Wendel <wendel4019@gmail.com>
2024-04-07 14:59:19 -0300

    closes #4, closes #5, closes #6 - Array clients, search by CEP and save logic

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+var registers = [];
+
 function search() {
     var cep = document.getElementById("cep").value;
     var url = `https://brasilapi.com.br/api/cep/v1/${cep}`;
-
+    
     fetch(url)
         .then(response => {
             if (!response.ok) {
@@ -13,7 +15,7 @@ function search() {
         })
         .then(address => {
             showAddress(address);
-            clearError()
+            clearError();
         })
         .catch(error => {
             showError("Endereço não encontrado");
@@ -34,6 +36,79 @@ function showAddress(address) {
     document.getElementById("neighborhood").value = address.neighborhood;
     document.getElementById("city").value = address.city;
     document.getElementById("state").value = address.state;
+}
+
+//OnLoad
+loadRegisters();
+
+//Load all studants
+function loadRegisters() {
+    for (let register of registers) {
+        addNewRow(register);
+    }
+}
+
+// Save students
+function save() {
+    var register = {
+        id: registers.length + 1,
+        firstName: document.getElementById("firstName").value,
+        lastName: document.getElementById("lastName").value,
+        number: document.getElementById("number").value,
+        street: document.getElementById("street").value,
+        neighborhood: document.getElementById("neighborhood").value,
+        city: document.getElementById("city").value,
+        state: document.getElementById("state").value
+    };
+
+     // Verifica se o endereço já está presente nos registros
+     var existingRegister = registers.find(existing => existing.street === register.street && existing.number === register.number);
+     if (existingRegister) {
+         showError("This address already exists!");
+         return; // Aborta a função para evitar a inserção do mesmo CEP novamente
+     }
+
+    registers.push(register); // Adiciona o registro ao array
+
+    addNewRow(register); // Adiciona o registro à tabela
+
+    // Reseta o formulário após salvar os dados
+    document.getElementById("formRegister").reset();
+}
+
+
+//Add new row
+function addNewRow(register) {
+    var table = document.getElementById("registerTable");
+    var newRow = table.insertRow(-1); // Insere a nova linha no final da tabela
+
+    // insere o Id na tabela
+    var idNode = document.createTextNode(register.id);
+    newRow.insertCell().appendChild(idNode);
+
+    // insere o Name na tabela
+    var nameNode = document.createTextNode(register.firstName + " " + register.lastName);
+    newRow.insertCell().appendChild(nameNode);
+
+    // insere o numero na tabela
+    var numberNode = document.createTextNode(register.number);
+    newRow.insertCell().appendChild(numberNode);
+
+    // insere o rua na tabela
+    var streetNode = document.createTextNode(register.street);
+    newRow.insertCell().appendChild(streetNode);
+
+    // insere o bairro na tabela
+    var neighborhoodNode = document.createTextNode(register.neighborhood);
+    newRow.insertCell().appendChild(neighborhoodNode);
+
+    // insere o cidade na tabela
+    var cityNode = document.createTextNode(register.city);
+    newRow.insertCell().appendChild(cityNode);
+
+    // insere o estado na tabela
+    var stateNode = document.createTextNode(register.state);
+    newRow.insertCell().appendChild(stateNode);
 }
 
 
